c5da1a1b4a8eae2b7645413cc256addae1e82634
twined <64403281+twinedd@users.noreply.github.com>
2025-03-22 22:56:11 +0300

    Update ngrok_bot.py

--- ngrok_bot.py
+++ ngrok_bot.py
@@ -8,6 +8,7 @@ from lib_mc2 import to_ansi_123
 
 token = ''
 bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
+servers = False
 
 @bot.event
 async def on_ready():
@@ -21,7 +22,7 @@ def create_embed_forscan():
 
 def create_embed_forfound(list_players, motd, players_online, version, ip):
     ServerInfo = requests.get(f'https://api.mcsrvstat.us/3/{ip}')
-    embed = discord.Embed(title="Succesfully founded server",description=f"**Server's description:** {motd}",color=discord.Colour.green(),)
+    embed = discord.Embed(title="Succesfully founded server",description=f"**Player's** {list_players}\n**Server's description:** {motd}",color=discord.Colour.green(),)
     embed.set_author(name = 'NgrokSeeker', icon_url ='https://github.com/twinedd/pngs/raw/main/green-wifi-15069.png')
     embed.add_field(name = "Server's ip:", value = f'```{ip}```')
     
@@ -43,9 +44,9 @@ def create_embed_forfound(list_players, motd, players_online, version, ip):
         except:
             pass
 
-        embed.add_field(name = "Server's stats:", value = f"**Version:** {version}\n**Player's count:** {players_online}\n**Player's** {list_players}\n**License:** {ServerJson['eula_blocked']}\n**Core**: {core}\n**Modded**: {mods}")
+        embed.add_field(name = "Server's stats:", value = f"**Version:** {version}\n**Player's count:** {players_online}\n**License:** {ServerJson['eula_blocked']}\n**Core**: {core}\n**Modded**: {mods}")
     else:
-        embed.add_field(name = "Server's stats:", value = f"**Version:** {version}\n**Player's count:** {players_online}\n**Player's** {list_players}\n")
+        embed.add_field(name = "Server's stats:", value = f"**Version:** {version}\n**Player's count:** {players_online}")
 
     return embed
 
@@ -56,22 +57,27 @@ def create_embed_forstop():
 
 @bot.slash_command(description="Starts scanning")
 async def start_scanning(ctx, port_range1 = 10000, port_range2 = 30000, version_check = 'any'):
-    list_servers = []
-    await ctx.respond(embed = create_embed_forscan())
-    for port in range(int(port_range1), int(port_range2)):
-        for host in range(1,8):
-            list_servers.append(f'{host}.tcp.eu.ngrok.io:{port}')
-    for i in range(1,10):
-        thread = threading.Thread(target = randomizer, args = (ctx, list_servers, version_check))
-        thread.start()
+    global servers
+    if not servers:
+        list_servers = []
+        await ctx.respond(embed = create_embed_forscan())
+        for port in range(int(port_range1), int(port_range2)):
+            for host in range(1,8):
+                list_servers.append(f'{host}.tcp.eu.ngrok.io:{port}')
+        for i in range(1,15):
+            thread = threading.Thread(target = randomizer, args = (ctx, list_servers, version_check))
+            thread.start()
+    else:
+        await ctx.respond('you cant start one more')
         
-servers = True
 
 def randomizer(ctx, list_servers, version_check):
     global servers
     servers = True
     while servers:
         for i in range(1,8):
+            if len(list_servers) % 50 == 0:
+                bot.loop.create_task(bot.change_presence(status=discord.Status.online, activity=discord.Activity(name=f" ngrok servers | current port: {list_servers[0].split(':')[1]}", type=3)))
             if not servers:
                 break
             try:
@@ -86,11 +92,12 @@ def randomizer(ctx, list_servers, version_check):
                     print('return')
                     return
                 bot.loop.create_task(ctx.send(embed = create_embed_forfound(list_players = list_players, motd = f'```ansi\n{to_ansi_123(Motd.parse(server.motd.raw).to_minecraft())}```', version = version, players_online = server.players.online, ip = ip)))
-            except:
-                pass
+            except Exception as s:
+                print(s)
 
 @bot.slash_command()
 async def stop_scanning(msg):
+    await bot.change_presence(status=discord.Status.online, activity=discord.Activity(name=" ngrok servers", type=3))
     global servers
     servers = False
     await msg.respond(embed = create_embed_forstop())
@@ -106,7 +113,7 @@ async def ping(msg, ip):
         version = str(server.version).split("'")[1]
         await msg.respond(embed = create_embed_forfound(list_players = list_players, motd = f'```ansi\n{to_ansi_123(Motd.parse(server.motd.raw).to_minecraft())}```', version = version, players_online = server.players.online, ip = ip))
     except Exception as s:
-        await msg.respond('the server is not responding', s)
+        await msg.respond('the server is not responding')
         pass
 
 bot.run(token)
